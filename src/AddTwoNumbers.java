1838ddef1ed973e8727a4d6a38195eae53554262
Ahobilanathan <ahobilanathan@gmail.com>
2023-02-23 18:16:17 +0530

    Add two number edit 1

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class AddTwoNumbers {
+public class AddTwoNumbers {
+    static void add(int a, int b)
+    {
+        System.out.println("The sum of a+b is : " + (a+b));
+    }
+
+    public static void main(String[] args) {
+        add(5, 2);
+    }
 }
